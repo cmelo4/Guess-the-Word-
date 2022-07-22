6e77669e8bfd14a0bb0baaa946ebdb8fbd3506df
cmelo4 <106715832+cmelo4@users.noreply.github.com>
2022-07-21 21:38:11 -0400

    Update Code.py
    updates the answer list

--- Code.py
+++ Code.py
@@ -60,7 +60,7 @@ hangman = ['''
   =========''']
 
 answerlist = [
-"dog", "cat", "girrafe", "elephant", "sheep", "horse", "cow", "crocodale", "snake", "jaguar", "lion", "tiger", "spider", "scorpion", "zebra", "falcon", "eagle", "frog", "shark", "seal", "whale"
+"dog", "cat", "giraffe", "elephant", "sheep", "horse", "cow", "crocodale", "snake", "jaguar", "lion", "tiger", "spider", "scorpion", "zebra", "falcon", "eagle", "frog", "shark", "seal", "whale"
            ]
 
 guess_word = []
